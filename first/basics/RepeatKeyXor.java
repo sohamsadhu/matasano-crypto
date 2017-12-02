dc7cd8e47a87507ac37f2cbbc3c5a2305a3ddd54
Soham Sadhu <soham@Soham.local>
2017-12-02 16:57:59 -0500

    Add problem 5

---
+++ first/basics/RepeatKeyXor.java
@@ -0,0 +1,40 @@
+// Problem 5
+
+public class RepeatKeyXor {
+
+  public byte[] getBytesFromString(String input) {
+    int length = input.length();
+    byte[] result = new byte[length];
+    for (int i = 0; i < length; i++) {
+      result[i] = (byte) (input.charAt(i) & 0x00FF);
+    }
+    return result;
+  }
+
+  public String getHexByRepeatXor(String input, String key, FixedXor fxor) {
+    int length = input.length();
+    byte[] inputBytes = getBytesFromString(input);
+    int keyLength = key.length();
+    byte[] xorKey = getBytesFromString(key);
+    byte[] result = new byte[length];
+    for (int i = 0; i < length; i++) {
+      int temp1 = (int) inputBytes[i];
+      int temp2 = (int) xorKey[i % keyLength];
+      int temp3 = temp1 ^ temp2;
+      result[i] = (byte) (temp3 & 0xFF);
+    }
+    return fxor.convertBytesToHexString(result);
+  }
+
+  public static void main(String [] args) {
+    RepeatKeyXor rxor = new RepeatKeyXor();
+    String input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    String key = "ICE";
+    String expectedOutput = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c"+
+      "2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286"+
+      "326302e27282f";
+    FixedXor fxor = new FixedXor();
+    String output = rxor.getHexByRepeatXor(input, key, fxor);
+    System.out.println("Conversion done: "+ output.equals(expectedOutput));
+  }
+}
