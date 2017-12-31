ac52382b3ade438ee92bfdc46f3a6f027d23f7b9
Soham Sadhu <sohamsadhu@gmail.com>
2017-12-30 21:27:20 -0500

    Add space frequency to obtain nearest string
    1. Add frequency of space in English language to calculate nearest
    matching string.
      1.a. Absence of space matrix allows the algorithm to pick up false
           positive results of nearest match. This sometimes produces
           readable but childish text.

--- first/basics/SingleByteXor.java
+++ first/basics/SingleByteXor.java
@@ -2,10 +2,16 @@
 
 public class SingleByteXor {
 
-  // https://en.wikipedia.org/wiki/Letter_frequency, Robert Lewand's Cryptological Mathematics
-  public static final double [] LETTER_FREQUENCY = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 
-    0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 
-    0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.0236, 0.0015, 0.01974, 0.00074};
+  // http://www.data-compression.com/english.html
+  // The following array holds the letter frequency for each character in english alphabet. Frequency
+  // of a letter can be obtained, by looking at index given by position of the letter in alphabet, in
+  // the given array. For example letter 'b' will have the frequency 0.0124248, obtained by looking in
+  // position LETTER_FREQUENCY[2], since 'b' is the second character in alphabet.
+  // Please note that there are 27 entries in below array. The 27th entry is for space character frequency.
+  public static final double [] LETTER_FREQUENCY = {0.0651738, 0.0124248, 0.0217339, 0.0349835 , 0.1041442,
+    0.0197881, 0.0158610, 0.0492888, 0.0558094, 0.0009033, 0.0050529, 0.0331490, 0.0202124, 0.0564513,
+    0.0596302, 0.0137645, 0.0008606, 0.0497563, 0.0515760, 0.0729357, 0.0225134, 0.0082903, 0.0171272,
+    0.0013692, 0.0145984, 0.0007836, 0.1918182};
 
   public byte[][] getEveryXor(byte[] input) {
     int xoredLength = input.length;
@@ -23,7 +29,7 @@ public class SingleByteXor {
 
   public double scoreByteToFrequency(byte[] input) {
     double countLetter = 0.0;
-    double [] count = new double[26];
+    double [] count = new double[27];
     double redundantChars = 0.0;
     for (byte letter : input) {
       int i = (int) letter;
@@ -31,12 +37,15 @@ public class SingleByteXor {
         int j = (i < 91) ? (i - 65) : (i - 97);
         count[j]++;
         countLetter++;
+      } else if (32 == i) {
+        count[26]++;
+        countLetter++;
       } else {
         redundantChars++;
       }
     }
     double result = 0.0;
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 27; i++) {
       if (count[i] > 0.0) {
         result += Math.pow((count[i] / countLetter) - LETTER_FREQUENCY[i], 2);
       }
@@ -69,18 +78,18 @@ public class SingleByteXor {
     return result;
   }
 
-  public String getHighestMatch(String hexInput) {
+  public CryptResult getHighestMatch(String hexInput) {
     HexToBase64 hb64 = new HexToBase64();
     byte[] hexBytes = hb64.convertHexToBytes(hexInput);
     byte[][] xoredBytes = getEveryXor(hexBytes);
-    CryptResult result = getLowestDeviationBytes(xoredBytes);
-    return getAsciiStringFromBytes(result.getExtractedBytes());
+    return getLowestDeviationBytes(xoredBytes);
   }
 
   public static void main(String [] args) {
     SingleByteXor sxor = new SingleByteXor();
     String hexInput = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    String output = sxor.getHighestMatch(hexInput);
-    System.out.println("Decoded string is: "+ output);
+    CryptResult result = sxor.getHighestMatch(hexInput);
+    String output = new String(result.getExtractedBytes());
+    System.out.println("Key is: "+ result.getSingleKey() +", decoded string is: "+ output);
   }
 }
--- first/basics/SingleCharXor.java
+++ first/basics/SingleCharXor.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 // Problem 4
 public class SingleCharXor {
 
-  public String getClosestString(String file, SingleByteXor sbxor, HexToBase64 hb64) {
-    String output = "";
+  public CryptResult getClosestString(String file, SingleByteXor sbxor, HexToBase64 hb64) {
+    CryptResult result = new CryptResult();
     double min = Double.MAX_VALUE;
     try (BufferedReader br = new BufferedReader(new FileReader(file))) {
       String line;
       while ((line = br.readLine()) != null) {
-        String s = sbxor.getHighestMatch(line);
+        CryptResult match = sbxor.getHighestMatch(line.trim());
         double score = sbxor.scoreByteToFrequency(hb64.convertHexToBytes(line));
         if (score < min) {
           min = score;
-          output = s;
+          result = match;
         }
       }
     } catch(IOException ioex) {
       ioex.printStackTrace();
     }
-    return output;
+    return result;
   }
 
   public static void main(String[] args) {
@@ -29,7 +29,8 @@ public class SingleCharXor {
     SingleByteXor sbxor = new SingleByteXor();
     HexToBase64 hb64 = new HexToBase64();
     String file = (args.length > 0) ? args[0] : "4.txt";
-    String outputFile = scxor.getClosestString(file, sbxor, hb64);
-    System.out.println("Closest matching string is: "+ outputFile);
+    CryptResult result = scxor.getClosestString(file, sbxor, hb64);
+    System.out.println("Decoded string is: "+ sbxor.getAsciiStringFromBytes(result.getExtractedBytes()) 
+      +", and key is "+ (char)result.getSingleKey());
   }
 }
