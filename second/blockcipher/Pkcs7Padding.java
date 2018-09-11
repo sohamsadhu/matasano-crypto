d073c16c6e2647e41133cb15e1d6ee094ffa18c4
Soham Sadhu <sohamsadhu@gmail.com>
2018-09-10 22:59:34 -0400

    refactor

--- second/blockcipher/Pkcs7Padding.java
+++ second/blockcipher/Pkcs7Padding.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Formatter;
-
 // Implement PKCS#7 padding
 // A block cipher transforms a fixed-sized block (usually 8 or 16 bytes) of plaintext into ciphertext. 
 // But we almost never want to transform a single block; we encrypt irregularly-sized messages.
@@ -8,6 +5,8 @@ import java.util.Formatter;
 // even multiple of the blocksize. The most popular padding scheme is called PKCS#7.
 // So: pad any block to a specific block length, by appending the number of bytes of padding to the end of the block. For instance,
 // "YELLOW SUBMARINE" ... padded to 20 bytes would be: "YELLOW SUBMARINE\x04\x04\x04\x04"
+
+import java.util.Arrays;
 
 public class Pkcs7Padding {
 
